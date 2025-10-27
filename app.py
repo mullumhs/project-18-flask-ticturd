78ffec86b7ecbf864b50c5aa28e6a786ad032dc1
ticturd <148925085+ticturd@users.noreply.github.com>
2025-10-28 09:49:42 +1100

    finished activity 4 of task 3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 app = Flask(__name__)
 
 
@@ -16,7 +17,6 @@ def hello(name):
 
 @app.route('/calc/<int:num1>/<string:operation>/<int:num2>')
 def calculator(num1, operation, num2):
-
 
     if operation == "add":
         result = (f"{num1 + num2}")
@@ -47,6 +47,23 @@ def search():
 
     return f'Searching for "{query}" in category: {category}'
 
+
+@app.route('/basic')
+def basic():
+    return render_template('basic.html')
+
+@app.route('/greet/<name>')
+def greet(name):
+    return render_template('greet.html', name=name)
+
+@app.route('/inventory')
+def inventory():
+    inventory_items = ['jegna', 'jimmy', 'e']
+    return render_template('inventory.html', inventory = inventory_items)
+
+@app.route('/test/<message>')
+def test(message):
+    return render_template('test.html', message = message)
 
 if __name__ == '__main__':
     app.run(debug=True)
